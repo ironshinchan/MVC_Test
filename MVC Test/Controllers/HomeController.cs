f8206d1f35ac707cb2cabdb6d667c285983e55fb
ironshinchan <jow.ramos@gmail.com>
2017-12-12 09:42:27 +0800

    Test

---
+++ MVC Test/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Test.Models;
+
+namespace MVC_Test.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetProducts()
+        {
+            JsonResult result = new JsonResult();
+
+            dataProductsDataContext dcProducts = new dataProductsDataContext();
+
+            result.Data = new { results = dcProducts.spc_GetProducts() };
+            result.JsonRequestBehavior = JsonRequestBehavior.AllowGet;
+
+            return result;
+        }
+    }
+}
